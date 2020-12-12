7f332bec8426714c4bf81f03878f2e6f1587a5c5
TheColdPot <guohw979@outlook.com>
2020-12-12 14:48:28 +0800

    Core: Update elementOperator.js

--- src/utils/elementOperator.js
+++ src/utils/elementOperator.js
@@ -1,7 +1,7 @@
 export default {
     setMultipleAttributes(target, objectSeq) {
         Object.keys(objectSeq).forEach((attrName) => {
-            target.setAttribute(attrName, objectSeq[attrName])
+            target?.setAttribute(attrName, objectSeq[attrName])
         });
     }
 }
