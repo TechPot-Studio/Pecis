3a895bda6c8a4c8c430e3a14871848a6a35261a4
TheColdPot <guohw979@outlook.com>
2020-12-19 22:34:42 +0800

    Docs: Merge pages

--- docs/pages/functions.js
+++ docs/pages/functions.js
@@ -4,18 +4,43 @@ if (/*@cc_on!@*/0) {
     window.close();
 }
 
-function switchPage (name) {
+function switchPage(name) {
     const loading = document.querySelector('#loading');
     loading.style.visibility = 'visible';
     document.querySelector('#content iframe').src = `${ver}/${name}.xml`
 }
-function switchVer () {
+
+function switchVer() {
     ver = document.querySelector('#select-ver').value;
+    document.querySelector('#content iframe').removeAttribute('src');
+    document.querySelectorAll('div.sidebar-content').forEach(each => each.style.display = 'none');
+    Swal.closeModal();
+    try {
+        document.querySelector(`div.sidebar-${ver}`).style.display = 'block';
+        Swal.fire({
+            text: `Docs version changed to ${ver} successfully!`,
+            position: 'top-left',
+            showConfirmButton: false,
+            toast: true,
+            timer: 3000
+        });
+    } catch (e) {
+        Swal.fire({
+            title: 'Oops!',
+            text: `We have not found the version ${ver}!`,
+            position: 'top-left',
+            showConfirmButton: false,
+            toast: true,
+            timer: 3000,
+            showCancelButton: true
+        });
+    }
     document.querySelectorAll('.docs-list').forEach(each => each.childNodes.forEach((each) => {
         each.addEventListener('click', function () {
             switchPage(each.innerText);
         })
-    }))
+    }));
+    return ver;
 }
 window.addEventListener('load', function () {
     const iframe = document.querySelector('#content iframe'),
