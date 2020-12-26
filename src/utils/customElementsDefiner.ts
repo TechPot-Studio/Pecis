5d6dca8b272f37072b048f0e70f1b462a57d7d96
TheColdPot <guohw979@outlook.com>
2020-12-26 12:47:55 +0800

    Core: Refractory elementDefiner since js to ts

---
+++ src/utils/customElementsDefiner.ts
@@ -0,0 +1,17 @@
+import haveDocument from './haveDocument';
+
+/**
+ * Define a custom element
+ */
+function defineCustomElement (name: string, constructor: CustomElementConstructor): void {
+    if (haveDocument()) {
+        customElements.define(name, constructor)
+    }
+}
+
+/**
+ * Define multiple elements
+ */
+export default function defineElements (...consequences: [string, CustomElementConstructor]) {
+    consequences.forEach(eachConsequence => defineCustomElement(eachConsequence[0], eachConsequence[1]));
+}
