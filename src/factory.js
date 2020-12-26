f265c894d655e44acdf301a7702bf0516c7202e3
TheColdPot <guohw979@outlook.com>
2020-12-26 13:53:57 +0800

    Build: Add resolve.extensions to webpack.config.js

--- src/factory.js
+++ src/factory.js
@@ -6,7 +6,7 @@ import ElementManager from './classes/ElementManager';
 import TranslationTable from './classes/TranslationTable';
 import * as methods from './methods';
 import PecisTransElement from './elements/PecisTransElement';
-import defineCustomElements from './utils/customElementsDefiner.ts';
+import defineCustomElements from './utils/customElementsDefiner';
 
 defineCustomElements(['pecis-trans', PecisTransElement]);
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'pecis.min.js'
     },
+    resolve: {
+        extensions: ['.js', '.ts', '.scss']
+    },
     module: {
         rules: [
             {
